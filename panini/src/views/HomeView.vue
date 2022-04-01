f598dd40118d0bd98f97febf67d8b49bc6d7000d
Carol Alves <carol.alves99@live.com>
2022-04-01 20:59:32 -0300

    Adiciona conteúdo da landing page

--- panini/src/views/HomeView.vue
+++ panini/src/views/HomeView.vue
@@ -4,4 +4,50 @@ import Header from "../components/Header.vue";
 
 <template>
   <Header :isUserLoggedIn="false" active-route="Home"></Header>
+
+  <MDBContainer>
+    <MDBRow class="bg-light">
+      <MDBCol class="text-info-col">
+        <h1 class="h1-responsive font-weight-bold text-left">
+          Panini
+        </h1>
+        <p>
+          A melhor plataforma para receber pães de todos os tipos da sua casa! Faça seu cadastro e conheça mais sobre nós!
+        </p>
+        <MDBBtn color="secondary">Faça seu cadastro <MDBIcon icon="chevron-right" /></MDBBtn>
+      </MDBCol>
+      <MDBCol class="landing-page-image-col d-none d-sm-block d-sm-none d-md-block">
+        <img src="../assets/landing-page-image.jpg" alt="Imagem de padeiro" class="landing-page-image">
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
 </template>
+
+<style>
+.landing-page-image {
+  width: 95%;
+  height: 95%;
+}
+
+.text-info-col {
+  padding-top: 20%;
+}
+
+h1 {
+  font-size: 4rem;
+}
+</style>
+
+<script>
+  import { MDBCol, MDBRow, MDBContainer, MDBBtn, MDBIcon  } from 'mdb-vue-ui-kit';
+
+  export default {
+    components: {
+      MDBCol,
+      MDBRow,
+      MDBContainer,
+      MDBBtn,
+      MDBIcon 
+    },
+  };
+</script>
