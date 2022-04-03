5ae8086de7a1aaeb633a1964a700150ed91842f0
Juarez Geraldo Silva Jr <juarez.junior@grupomult.com.br>
2022-04-03 16:55:42 -0300

    Informações complementares

--- panini/src/views/CadastroView.vue
+++ panini/src/views/CadastroView.vue
@@ -13,37 +13,37 @@ import HeaderComponent from '../components/HeaderComponent.vue';
         <h2 class="h2-responsive font-weight-bold text-left">Cadastro</h2>
         <p>Informe seus dados para poder fazer parte do seleto grupo de pessoas que a Panini irá atender</p>
         <br />
-        <MDBInput label="Informe seu nome" v-model="nome" />
+        <MDBInput label="Informe seu nome" v-model="cadastroPessoa.nome" />
         <br />
-        <MDBInput label="Informe seu e-mail" v-model="email" type="email" />
+        <MDBInput label="Informe seu e-mail" v-model="cadastroPessoa.email" type="email" />
         <br />
         <MDBRow>
           <MDBCol md="3">
-            <MDBInput label="CEP" v-model="cep" type="number" />
+            <MDBInput label="CEP" v-model="cadastroPessoa.cep" type="number" />
           </MDBCol>
           <MDBCol md="7">
-            <MDBInput label="Cidade" v-model="cidade" type="text" />
+            <MDBInput label="Cidade" v-model="cadastroPessoa.cidade" type="text" />
           </MDBCol>
           <MDBCol md="2">
-            <MDBInput label="UF" v-model="uf" type="text" />
+            <MDBInput label="UF" v-model="cadastroPessoa.uf" type="text" />
           </MDBCol>
         </MDBRow>
         <br />
         <MDBRow>
           <MDBCol md="8">
-            <MDBInput label="Logradouro" v-model="logradouro" type="text" />
+            <MDBInput label="Logradouro" v-model="cadastroPessoa.logradouro" type="text" />
           </MDBCol>
           <MDBCol md="4">
-            <MDBInput label="Número" v-model="cidade" type="text" />
+            <MDBInput label="Número" v-model="cadastroPessoa.numero" type="text" />
           </MDBCol>
         </MDBRow>
         <br />
         <MDBRow>
           <MDBCol md="6">
-            <MDBInput label="Complemento" v-model="complemento" type="text" />
+            <MDBInput label="Complemento" v-model="cadastroPessoa.complemento" type="text" />
           </MDBCol>
           <MDBCol md="6">
-            <MDBInput label="Bairro" v-model="bairro" type="text" />
+            <MDBInput label="Bairro" v-model="cadastroPessoa.bairro" type="text" />
           </MDBCol>
         </MDBRow>
         <br />
@@ -55,6 +55,11 @@ import HeaderComponent from '../components/HeaderComponent.vue';
           Cancelar cadastro
           <MDBIcon icon="chevron-right" />
         </MDBBtn>
+        <div v-show="flagErro">
+          <br />
+          {{ flagErro }}
+          <div class="alert alert-danger">*** {{ mensagemErro }} ***</div>
+        </div>
       </MDBCol>
       <MDBCol class="landing-page-image-col d-none d-sm-block d-sm-none d-md-block">
         <img
@@ -84,17 +89,11 @@ h1 {
 <script>
 import { MDBCol, MDBRow, MDBContainer, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-vue-ui-kit';
 import { ref } from 'vue';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  data() {
-    return {
-      ingredientes: [
-        { id: 1, caixa: "checkbox1", nome: "Farinha integral enriquecida" },
-        { id: 2, caixa: "checkbox2", nome: "Farinha integral" },
-        { id: 3, caixa: "checkbox3", nome: "Farinha normal" },
-      ]
-    }
-  },
+  name: "salvarCadastro",
+
   components: {
     MDBCol,
     MDBRow,
@@ -114,9 +113,6 @@ export default {
     const numero = ref(0);
     const complemento = ref('');
     const bairro = ref('');
-    const checkbox1 = ref(false);
-    const checkbox2 = ref(false);
-    const checkbox3 = ref(false);
     return {
       nome,
       email,
@@ -127,16 +123,109 @@ export default {
       numero,
       complemento,
       bairro,
-      checkbox1,
-      checkbox2,
-      checkbox3
     };
   },
+  data() {
+    return {
+      mensagemErro: "",
+      flagErro: false,
+      cadastroPessoa: {
+        id: null,
+        nome: "",
+        email: "",
+        cep: 0,
+        cidade: "",
+        uf: "",
+        logradouro: "",
+        numero: "",
+        complemento: "",
+        bairro: "",
+        ingredientes: {
+          farinhaComum: false,
+          farinhaIntegral: false,
+          farinhaIntegralEnriquecida: false,
+          acucarBranco: false,
+          acucarMascavo: false,
+          adocante: false,
+          queijos: false,
+          frutasCristalizadas: false,
+          ervasEspeciarias: false,
+        },
+        formas: {
+          pequenoIndividual: false,
+          medio4Pessoas: false,
+          grande8Pessoas: false,
+          redondo: false,
+          comprido: false,
+          quadrado: false,
+          corClara: false,
+          corMedia: false,
+          corEscura: false,
+        },
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(["flagInclui", "ultimoId"]),
+  },
   methods: {
+    ...mapActions(["incluirPessoa", "setFlagInclui", "incrementarId"]),
     salvarCadastro() {
-      this.$router.push({
-        name: "informacoes",
-      });
+      if (this.criticarCampos()) {
+        let dados = {
+          id: this.ultimoId,
+          nome: this.cadastroPessoa.nome,
+          email: this.cadastroPessoa.email,
+          cep: this.cadastroPessoa.cep,
+          cidade: this.cadastroPessoa.cidade,
+          uf: this.cadastroPessoa.uf,
+          logradouro: this.cadastroPessoa.logradouro,
+          numero: this.cadastroPessoa.numero,
+          complemento: this.cadastroPessoa.complemento,
+          bairro: this.cadastroPessoa.bairro,
+          ingredientes: {
+            farinhaComum: false,
+            farinhaIntegral: false,
+            farinhaIntegralEnriquecida: false,
+            acucarBranco: false,
+            acucarMascavo: false,
+            adocante: false,
+            queijos: false,
+            frutasCristalizadas: false,
+            ervasEspeciarias: false,
+          },
+          formas: {
+            pequenoIndividual: false,
+            medio4Pessoas: false,
+            grande8Pessoas: false,
+            redondo: false,
+            comprido: false,
+            quadrado: false,
+            corClara: false,
+            corMedia: false,
+            corEscura: false,
+          },
+        };
+        this.incluirPessoa(dados);
+        this.incrementarId();
+        this.registroIncluido = true;
+        this.flagErro = false;
+        this.$router.push({
+          name: "informacoes",
+          params: { id: dados.id }
+        });
+      } else {
+        this.mensagemErro = "Todos os campos devem ser preenchidos...";
+        this.flagErro = true;
+      }
+    },
+    criticarCampos() {
+      return (
+        this.cadastroPessoa.nome &&
+        this.cadastroPessoa.email &&
+        this.cadastroPessoa.cidade &&
+        this.cadastroPessoa.logradouro
+      );
     },
     cancelarCadastro() {
       this.$router.push({
--- panini/src/views/InformacoesView.vue
+++ panini/src/views/InformacoesView.vue
@@ -4,7 +4,7 @@ import HeaderComponent from "../components/HeaderComponent.vue";
 
 <template>
   <!-- <Header :isUserLoggedIn="false" active-route="Home"></Header> -->
-  <HeaderComponent :isUserLoggedIn="false" active-route="Home"></HeaderComponent> 
+  <HeaderComponent :isUserLoggedIn="false" active-route="Home"></HeaderComponent>
 
   <MDBContainer>
     <MDBRow class="bg-light">
@@ -12,22 +12,36 @@ import HeaderComponent from "../components/HeaderComponent.vue";
         <h1 class="h1-responsive font-weight-bold text-left">Panini</h1>
         <p>A melhor plataforma para receber pães de todos os tipos da sua casa! Faça seu cadastro e conheça mais sobre nós!</p>
         <h2 class="h2-responsive font-weight-bold text-left">Informações complementares</h2>
-        <p>Estas informações serão utilizadas na personalização das cestas de pães e sugestões de produtos que a Panini poderá lhe fazer</p>
+        <p><span class="display-6"><strong>{{cadastroPessoa.nome}}</strong></span>, estas informações serão utilizadas na personalização das cestas de pães e sugestões de produtos que a Panini poderá lhe fazer</p>
         <br />
         <h3 class="h2-responsive font-weight-bold text-left">Ingredientes preferidos</h3>
-        <!-- <div v-for="ingrediente in ingredientes" :key="ingrediente.id"> -->
-              <MDBCheckbox inline label="Farinha comum" v-model="checkbox1" />
-              <MDBCheckbox inline label="Farinha integral" v-model="checkbox2" />
-              <MDBCheckbox inline label="Farinha integral enriquecida" v-model="checkbox3" />
-              <MDBCheckbox inline label="Farinha comum" v-model="checkbox3" />
-
-
-
-<!-- <div id="app"> -->
-          <!-- <blog-post v-for="ingrediente in ingredientes" :key="ingrediente.id"></blog-post> -->
-        <!-- </div> -->
+        <div>
+          <MDBCheckbox inline label="Farinha comum" v-model="cadastroPessoa.ingredientes.farinhaComum" />
+          <MDBCheckbox inline label="Farinha integral" v-model="cadastroPessoa.ingredientes.farinhaIntegral" />
+          <MDBCheckbox inline label="Farinha integral enriquecida" v-model="cadastroPessoa.ingredientes.farinhaIntegralEnriquecida" />
+          <MDBCheckbox inline label="Açucar branco" v-model="cadastroPessoa.ingredientes.acucarBranco" />
+          <MDBCheckbox inline label="Açucar macavo" v-model="cadastroPessoa.ingredientes.acucarMascavo" />
+          <MDBCheckbox inline label="Adoçante" v-model="cadastroPessoa.ingredientes.adocante" />
+          <MDBCheckbox inline label="Queijos" v-model="cadastroPessoa.ingredientes.queijos" />
+          <MDBCheckbox inline label="Frutas cristalizadas" v-model="cadastroPessoa.ingredientes.frutasCristalizadas" />
+          <MDBCheckbox inline label="Ervas e especiarias" v-model="cadastroPessoa.ingredientes.ervasEspeciarias" />
+        </div>
         <br />
-        <MDBBtn color="secondary" @click="salvarCadastro()">
+        <br />
+        <h3 class="h2-responsive font-weight-bold text-left">Formas e tamanhos</h3>
+        <div>
+          <MDBCheckbox inline label="Pequeno - individual" v-model="cadastroPessoa.formas.pequenoIndividual" />
+          <MDBCheckbox inline label="Médio - 4 pessoas" v-model="cadastroPessoa.formas.medio4Pessoas" />
+          <MDBCheckbox inline label="Grande - 8 pessoas" v-model="cadastroPessoa.formas.grande8Pessoas" />
+          <MDBCheckbox inline label="Redondo" v-model="cadastroPessoa.formas.redondo" />
+          <MDBCheckbox inline label="Comprido" v-model="cadastroPessoa.formas.comprido" />
+          <MDBCheckbox inline label="Quadrado" v-model="cadastroPessoa.formas.quadrado" />
+          <MDBCheckbox inline label="Cor clara" v-model="cadastroPessoa.formas.corClara" />
+          <MDBCheckbox inline label="Cor média" v-model="cadastroPessoa.formas.corMedia" />
+          <MDBCheckbox inline label="Cor escura" v-model="cadastroPessoa.formas.corEscura" />
+        </div>
+        <br />
+        <MDBBtn color="secondary" @click="salvarInformacoes()">
           Salvar informaões complementares
           <MDBIcon icon="chevron-right" />
         </MDBBtn>
@@ -64,34 +78,13 @@ h1 {
 
 <script>
 
-// const App = {
-//     data() {
-//     return {}
-
-// }}
-// const app = Vue.createApp(App)
-// app.component(
-//   'blog-post', {
-//   props: ['title', 'text'],
-//   template: `
-//                   <MDBCheckbox label="$ingrediente.nome"
-//           v-model="$ingrediente.caixa" />
-// `
-// })
-
 import { MDBCol, MDBRow, MDBContainer, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-vue-ui-kit';
 import { ref } from 'vue';
+import { mapActions, mapGetters } from "vuex";
+
 
 export default {
-  data() {
-    return {
-      ingredientes: [
-        { id: 1, caixa: "checkbox1", nome: "Farinha integral enriquecida" },
-        { id: 2, caixa: "checkbox2", nome: "Farinha integral" },
-        { id: 3, caixa: "checkbox3", nome: "Farinha normal" },
-      ]
-    }
-  },
+  name: "salvarInformacoes",
   components: {
     MDBCol,
     MDBRow,
@@ -101,20 +94,26 @@ export default {
     MDBIcon,
     MDBCheckbox
   },
-  setup() {
-    // const nome = ref('');
-    // const email = ref('');
-    // const cep = ref(0);
-    // const cidade = ref('');
-    // const uf = ref('');
-    // const logradouro = ref(0);
-    // const numero = ref(0);
-    // const complemento = ref('');
-    // const bairro = ref('');
-    const checkbox1 = ref(false);
-    const checkbox2 = ref(false);
-    const checkbox3 = ref(false);
-    return {
+  // setup() {
+    // const chckIngr1 = ref(false);
+    // const chckIngr2 = ref(false);
+    // const chckIngr3 = ref(false);
+    // const chckIngr4 = ref(false);
+    // const chckIngr5 = ref(false);
+    // const chckIngr6 = ref(false);
+    // const chckIngr7 = ref(false);
+    // const chckIngr8 = ref(false);
+    // const chckIngr9 = ref(false);
+    // const chckText1 = ref(false);
+    // const chckText2 = ref(false);
+    // const chckText3 = ref(false);
+    // const chckText4 = ref(false);
+    // const chckText5 = ref(false);
+    // const chckText6 = ref(false);
+    // const chckText7 = ref(false);
+    // const chckText8 = ref(false);
+    // const chckText9 = ref(false);
+    // return {
       // nome,
       // email,
       // cep,
@@ -124,15 +123,97 @@ export default {
       // numero,
       // complemento,
       // bairro,
-      checkbox1,
-      checkbox2,
-      checkbox3
-    };
+      // chckIngr1,
+      // chckIngr2,
+      // chckIngr3,
+      // chckIngr4,
+      // chckIngr5,
+      // chckIngr6,
+      // chckIngr7,
+      // chckIngr8,
+      // chckIngr9,
+      // chckText1,
+      // chckText2,
+      // chckText3,
+      // chckText4,
+      // chckText5,
+      // chckText6,
+      // chckText7,
+      // chckText8,
+      // chckText9,
+  //   };
+  // },
+  data() {
+    return {
+      // mensagemErro: "",
+      // flagErro: false,
+      // pessoa: {
+      //   id: null,
+      //   nome: "",
+      //   email: "",
+      //   cep: 0,
+      //   cidade: "",
+      //   uf: "",
+      //   logradouro: "",
+      //   numero: "",
+      //   complemento: "",
+      //   bairro: "",
+      //   ingredientes: {
+      //     farinhaComum: false,
+      //     farinhaIntegral: false,
+      //     farinhaIntegralEnriquecida: false,
+      //     acucarBranco: false,
+      //     acucarMascavo: false,
+      //     adocante: false,
+      //     queijos: false,
+      //     frutasCristalizadas: false,
+      //     ervasEspeciarias: false,
+      //   },
+      //   formas: {
+      //     pequenoIndividual: false,
+      //     medio4Pessoas: false,
+      //     grande8Pessoas: false,
+      //     redondo: false,
+      //     comprido: false,
+      //     quadrado: false,
+      //     corClara: false,
+      //     corMedia: false,
+      //     corEscura: false,
+      //   }
+      // }
+    }
+  },
+  created() {
+    this.buscarPessoa(this.$route.params.id);
+  },
+  computed: {
+    ...mapGetters(["cadastroPessoa"]),
   },
   methods: {
-    cadastro() {
+    ...mapActions(["alterarPessoa", "buscarPessoa"]),
+    salvarInformacoes() {
+      // this.cadastroPessoa.ingredientes.farinhaComum = this.ingredientes.farinhaComum;
+      // this.cadastroPessoa.ingredientes.farinhaIntegral = this.ingredientes.farinhaIntegral;
+      // this.cadastroPessoa.ingredientes.farinhaIntegralEnriquecida = this.ingredientes.farinhaIntegralEnriquecida;
+      // this.cadastroPessoa.ingredientes.acucarBranco = this.ingredientes.acucarBranco;
+      // this.cadastroPessoa.ingredientes.acucarMascavo = this.ingredientes.acucarMascavo;
+      // this.cadastroPessoa.ingredientes.adocante = this.ingredientes.adocante;
+      // this.cadastroPessoa.ingredientes.queijos = this.ingredientes.queijos;
+      // this.cadastroPessoa.ingredientes.frutasCristalizadas = this.ingredientes.frutasCristalizadas;
+      // this.cadastroPessoa.ingredientes.ervasEspeciarias = this.ingredientes.ervasEspeciarias;
+
+      // this.cadastroPessoa.formas.pequenoIndividual = this.formas.pequenoIndividual;
+      // this.cadastroPessoa.formas.medio4Pessoas = this.formas.medio4Pessoas;
+      // this.cadastroPessoa.formas.grande8Pessoas = this.formas.grande8Pessoas;
+      // this.cadastroPessoa.formas.redondo = this.formas.redondo;
+      // this.cadastroPessoa.formas.comprido = this.formas.comprido;
+      // this.cadastroPessoa.formas.quadrado = this.formas.quadrado;
+      // this.cadastroPessoa.formas.corClara = this.formas.corClara;
+      // this.cadastroPessoa.formas.corMedia = this.formas.corMedia;
+      // this.cadastroPessoa.formas.corEscura = this.formas.corEscura;
+      this.alterarPessoa(this.cadastroPessoa);
       this.$router.push({
-        name: "cadastro",
+        name: "home",
       });
     },
     cancelarInformacoes() {
